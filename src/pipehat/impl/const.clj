ce9ca6d415f7b24b5a1210e8ee89f3c12f3e2cab
Eero Helenius <eero.helenius@solita.fi>
2023-04-04 07:42:30 +0300

    Use hexadecimal numbers for consts

--- src/pipehat/impl/const.clj
+++ src/pipehat/impl/const.clj
@@ -12,8 +12,8 @@
   (update-vals default-encoding-characters char)
   ,,,)
 
-(def ^{:const true :doc "The HL7 Minimal Lower Layer Protocol (MLLP) start-of-block character."} SB 11)
-(def ^{:const true :doc "The HL7 Minimal Lower Layer Protocol (MLLP) end-of-block character."} EB 28)
-(def ^{:const true :doc "The ASCII carriage return character."} CR 13)
+(def ^{:const true :doc "The HL7 Minimal Lower Layer Protocol (MLLP) start-of-block character."} SB 0x0B)
+(def ^{:const true :doc "The HL7 Minimal Lower Layer Protocol (MLLP) end-of-block character."} EB 0x1C)
+(def ^{:const true :doc "The ASCII carriage return character."} CR 0x0D)
 (def ^{:const true :doc "An int indicating the end of stream."} EOS -1)
 (def ^{:const true :doc "An int indicating the start of element."} SOE -2)
