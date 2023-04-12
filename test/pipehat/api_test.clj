53edd04430788e1f0b2c416387f52d6896228826
Eero Helenius <eero.helenius@solita.fi>
2023-04-12 18:36:53 +0300

    Update test names

--- test/pipehat/api_test.clj
+++ test/pipehat/api_test.clj
@@ -296,12 +296,12 @@
   []
   (filter #(string/ends-with? (.getName %) ".hl7") (.listFiles (io/file "samples"))))
 
-(deftest samples
+(deftest roundtrip-samples
   (doseq [file (sample-files)]
     (let [input (slurp (io/reader file))]
       (is (= input (sut/write-str (sut/read-str input)))))))
 
-(defspec roundtrip 25
+(defspec roundtrip-generative 25
   (prop/for-all [message (spec/gen ::specs/message)]
     (= message (sut/read-str (sut/write-str message)))))
 
